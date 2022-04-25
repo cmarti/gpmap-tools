aa85bb0f0dd283e723e406d2b80f5d626995d71a
Carlos Marti <carlos199m1@gmail.com>
2022-04-25 16:12:43 -0400

    bin dir corrected

--- gpmap/src/settings.py
+++ gpmap/src/settings.py
@@ -5,7 +5,7 @@ VERSION = '0.1.0'
 
 # Directories
 BASE_DIR = abspath(join(dirname(__file__), '..'))
-BIN_DIR = join(BASE_DIR, 'bin')
+BIN_DIR = join(BASE_DIR, '..', 'bin')
 TEST_DATA_DIR = join(BASE_DIR, 'test', 'data')
 
 # File paths
