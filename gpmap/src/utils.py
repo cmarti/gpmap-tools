27c46e67801835af0f181786b252ac3055bf3ad0
Carlos Marti <carlos199m1@gmail.com>
2022-06-28 09:27:20 -0400

    unnecessary functions removed

--- gpmap/src/utils.py
+++ gpmap/src/utils.py
@@ -31,14 +31,6 @@ def get_sparse_diag_matrix(values):
     return(m)
 
 
-def logit(p):
-    return(np.log(p  /(1 - p)))
-
-
-def invlogit(x):
-    return(np.exp(x) / (1  + np.exp(x)))
-
-
 class LogTrack(object):
     '''Logger class'''
 
@@ -66,109 +58,6 @@ def write_log(log, msg):
         log.write(msg)
 
 
-def generate_possible_sequences(l, alphabet=NUCLEOTIDES):
-    for seq in itertools.product(alphabet, repeat=l):
-        yield(''.join(seq))
-
-
-def reverse_complement(seq):
-    return(''.join(COMPLEMENT.get(x, x) for x in seq[::-1]))
-
-
-def get_random_seq(length):
-    return(''.join(np.random.choice(NUCLEOTIDES, size=length)))
-
-
-def add_random_flanks(seq, length, only_upstream=False):
-    if only_upstream:
-        flank = get_random_seq(length)
-        new_seq = flank + seq
-    else:
-        flanks = get_random_seq(2 * length)
-        new_seq = flanks[:length] + seq + flanks[length:]
-    return(new_seq)
-
-
-def write_landscape(landscape_iter, fpath, verbose=False):
-    with open(fpath, 'w') as fhand:
-        fhand.write('sequence\tdG\tKa\tP_helix\n')
-        for i, (seq, dG, Ka, p_helix) in enumerate(landscape_iter):
-            fhand.write('\t'.join([seq, str(dG), str(Ka), str(p_helix)]) + '\n')
-            if verbose and i % 10000 == 0:
-                total_seqs = len(NUCLEOTIDES) ** len(seq)
-                print('Sequences processed: {} out of {}'.format(i, total_seqs)) 
-
-
-def get_constraints_idx(constraints):
-    c1, c2 = constraints
-    idx1 = [i for i, x in enumerate(c1) if x == '(']
-    idx2 = [i for i, x in enumerate(c2[::-1]) if x == ')']
-    return(idx1, idx2)
-
-
-def get_seq(genome, chrom, start, end, strand):
-    seq = genome.fetch(chrom, start, end)
-    if strand == '-':
-        seq = reverse_complement(seq)
-    
-    return(seq.upper())
-
-
-def load_pickle(fpath):
-    with open(fpath, 'rb') as fhand:
-        data = load(fhand)
-    return(data)
-
-
-def write_pickle(data, fpath):
-    with open(fpath, 'wb') as fhand:
-        dump(data, fhand)
-    return(data)
-
-
-def get_model(model_label, recompile=False):
-    code_fpath = join(CODE_DIR, '{}.stan'.format(model_label))
-    compiled_fpath = join(MODELS_DIR, '{}.p'.format(model_label))
-
-    if not exists(compiled_fpath) or recompile:
-        # STAN model compilation
-        model = pystan.StanModel(file=code_fpath)
-        write_pickle(model, compiled_fpath)
-    else:
-        # Load stored model
-        model = load_pickle(compiled_fpath)
-    return(model)
-
-
-def translante_seqs(seqs, codon_table='Standard'):
-    prot_genotypes = np.array([str(Seq(seq).translate(table=codon_table))
-                               for seq in seqs])
-    return(prot_genotypes)
-
-
-def guess_configuration(seqs):
-    alleles = defaultdict(dict)
-    for seq in seqs:
-        for i, a in enumerate(seq):
-            alleles[i][a] = 1 
-    length = len(alleles)
-    config = {'length': length,
-              'n_alleles': [len(alleles[i]) for i in range(length)],
-              'alphabet': [[a for a in alleles[i].keys()]
-                           for i in range(length)]}
-    exp_n_seqs = np.prod(config['n_alleles'])
-    if exp_n_seqs > seqs.shape[0]:
-        msg = 'Number of genotypes is lower than the expected from guessed'
-        msg += ' configuration. Ensure that genotypes span the whole sequence '
-        msg += 'space'
-        raise ValueError(msg)
-    elif exp_n_seqs < seqs.shape[0]:
-        msg = 'Number of genotypes is higher than the expected from guessed'
-        msg += ' configuration'
-        Warning(msg)
-    return(config)
-
-
 def check_eigendecomposition(matrix, eigenvalues, right_eigenvectors, tol=1e-3):
     for l, u in zip(eigenvalues, right_eigenvectors.T):
         v1 = matrix.dot(u)
@@ -177,4 +66,3 @@ def check_eigendecomposition(matrix, eigenvalues, right_eigenvectors, tol=1e-3):
 
         msg = 'Numeric error in eigendecomposition: abs error = {:.5f} > {:.5f}'
         check_error(abs_err <= tol, msg.format(abs_err, tol))
-
