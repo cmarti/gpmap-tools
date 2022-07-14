039c77b5a794b07b0b4c986d3566636d87db4ec2
Carlos Marti <carlos199m1@gmail.com>
2022-07-14 14:51:27 -0400

    binary for seqdeft adapted to the changes in the API

--- bin/fit_seqdeft.py
+++ bin/fit_seqdeft.py
@@ -3,12 +3,13 @@ import argparse
 
 import pandas as pd
 
-from gpmap.utils import LogTrack
-from gpmap.inference import SeqDEFT
+from gpmap.src.utils import LogTrack
+from gpmap.src.inference import SeqDEFT
+from gpmap.src.plot import plot_SeqDEFT_summary, savefig
 
         
 def main():
-    description = 'Runs sequence density estimation using Field Theory (SeqDEFT)'
+    description = 'Runs Sequence Density Estimation using Field Theory (SeqDEFT)'
     description += ' from a table with the number of time each sequence was'
     description += ' observed e.g. the number of times each splice donor or'
     description += ' acceptor is found in the human genome'
@@ -18,18 +19,15 @@ def main():
     input_group = parser.add_argument_group('Input')
     input_group.add_argument('counts', help='CSV table with sequence counts')
 
-    options_group = parser.add_argument_group('Options')
+    options_group = parser.add_argument_group('SeqDEFT options')
     help_msg = 'Value of the hyperparameter a for SeqDEFT. If not provided, '
     help_msg += 'grid search with cross-validated likelihood will be used'
-    options_group.add_argument('-a', '--a_value', default=None,
+    options_group.add_argument('-a', '--a_value', default=None, type=float,
                                help=help_msg)
-    help_msg = 'P for the delta_P operator to build prior (2)'
+    options_group.add_argument('-k', '--num_a', default=20, type=int,
+                               help='Number of a values to test (20)')
+    help_msg = 'P for the Delta(P) operator to build prior (2)'
     options_group.add_argument('-P', '--delta_P', default=2, help=help_msg)
-
-    options_group.add_argument('-A', '--alphabet_type', default='dna',
-                               help='Alphabet type [dna, rna, protein, custom] (dna)')
-    options_group.add_argument('-n', '--n_alleles', default=None,
-                               help='Number of alleles to use for custom alphabet')
 
     output_group = parser.add_argument_group('Output')
     output_group.add_argument('-o', '--output', required=True,
@@ -38,25 +36,26 @@ def main():
     # Parse arguments
     parsed_args = parser.parse_args()
     counts_fpath = parsed_args.counts
-    alphabet_type = parsed_args.alphabet_type
-    n_alleles = parsed_args.n_alleles
+
     P = parsed_args.delta_P
-    a = parsed_args.a_value
+    a_value = parsed_args.a_value
+    num_a = parsed_args.num_a
+    
     out_fpath = parsed_args.output
     
     # Load counts data
     log = LogTrack()
     log.write('Start analysis')
     data = pd.read_csv(counts_fpath, index_col=0)
-    length = len(data.index[0])
-    print(length)
 
     # Load annotation data
-    seqdeft = SeqDEFT(length, P, alphabet_type=alphabet_type,
-                      n_alleles=n_alleles, log=log)
-    seqdeft.fit(data['counts'], resolution=0.1, max_a_max=1e6, num_a=50)
-    seqdeft.plot_summary(fname=out_fpath)
-    seqdeft.write_output(out_fpath)
+    seqdeft = SeqDEFT(P)
+    result = seqdeft.fit(X=data.index.values, counts=data['counts'].values,
+                         a_value=a_value, num_a=num_a)
+    result.to_csv(out_fpath)
+    
+    fig = plot_SeqDEFT_summary(seqdeft.log_Ls, result)
+    savefig(fig, out_fpath)
     
     log.finish()
 
