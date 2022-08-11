2a183c2862ee91f9d6ff59fd8e5c69f346939807
Carlos Marti <carlos199m1@gmail.com>
2022-08-11 19:24:03 -0400

    theme removed

--- docs/conf.py
+++ docs/conf.py
@@ -57,10 +57,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_pdj_theme
-#html_theme = 'sphinx_pdj_theme'
 html_theme = 'sphinx_rtd_theme'
-#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
