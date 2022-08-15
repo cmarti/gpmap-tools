9814ae91c8781faa60038a28c59d5361ec4f9825
Carlos Marti <carlos199m1@gmail.com>
2022-08-15 11:27:09 -0400

    attribute name fixed

--- gpmap/src/inference.py
+++ gpmap/src/inference.py
@@ -487,8 +487,8 @@ class VCregression(LandscapeEstimator):
         return np.array(post_vars)
     
     def calc_eigenvalue_multiplicity(self):
-        self.lambdas_multiplicity = np.array([comb(self.length, k) * (self.n_alleles - 1) ** k
-                                              for k in range(self.length + 1)], dtype=int)
+        self.lambdas_multiplicity = np.array([comb(self.seq_length, k) * (self.n_alleles - 1) ** k
+                                              for k in range(self.seq_length + 1)], dtype=int)
     
     def lambdas_to_variance(self, lambdas):
         self.calc_eigenvalue_multiplicity()
