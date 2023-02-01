problem in eigenvalue calculation fixed gradient usage for full kernel alignment started

gpmap/src/kernel.py
@@ -10,7 +10,7 @@ from gpmap.src.utils import check_error
 
 
 class SequenceKernel(object):
-    def __init__(self, n_alleles, seq_length):
+    def __init__(self, seq_length, n_alleles):
         self.alpha = n_alleles
         self.l = seq_length
         self.lp1 = self.l + 1
@@ -34,12 +34,15 @@ class SequenceKernel(object):
             self.set_extra_data(**kwargs)
     
     def __call__(self, **kwargs):
-        return(self.forward( **kwargs))
+        return(self.forward(**kwargs))
+    
+    def grad(self, **kwargs):
+        return(self.backward(**kwargs))
 
 
 class VarianceComponentKernel(SequenceKernel):
-    def __init__(self, n_alleles, seq_length, q=None, use_p=False):
-        super().__init__(n_alleles=n_alleles, seq_length=seq_length)
+    def __init__(self, seq_length, n_alleles, q=None, use_p=False):
+        super().__init__(seq_length=seq_length, n_alleles=n_alleles)
         self.use_p = use_p
         
         if self.use_p:
@@ -55,6 +58,10 @@ class VarianceComponentKernel(SequenceKernel):
             self.log_odds = log_q_powers - log_1mq_powers
         else:
             self.calc_krawchouk_matrix()
+            
+        self.n_params = self.lp1
+        if use_p:
+            self.n_params += self.l * self.alpha
     
     def calc_polynomial_coeffs(self):
         k = np.arange(self.lp1)
@@ -116,13 +123,32 @@ class VarianceComponentKernel(SequenceKernel):
             cov = self.W_kd.dot(lambdas)[hamming_distance]
         return(cov)
     
+    def backward(self):
+        if self.use_p:
+            raise ValueError('Not implemented for variable p')
+        else:
+            hamming_distance = self.calc_hamming_distance(self.x1, self.x2)
+            grad = [self.W_kd[k, :][hamming_distance] for k in range(self.lp1)]
+        return(grad)
+        
+
+    def get_params0(self):
+        params = np.append([-10], 2-np.arange(self.l))
+        if self.use_p:
+            params = np.append(params, np.zeros(self.l * self.alpha))
+        return(params)
+    
     def split_params(self, params):
         params_dict = {}
         if self.use_p:
-            params_dict['log_lambdas'] = params[:self.lp1]
-            params_dict['log_ps'] = params[self.lp1:].reshape(self.l, self.alpha)
+            params_dict['lambdas'] = np.exp(params[:self.lp1])
+            log_ps = params[self.lp1:].reshape(self.l, self.alpha)
+            norm_factors = logsumexp(log_ps, axis=1)
+            for i in range(log_ps.shape[1]):
+                log_ps[:, i] -= log_ps[:, i] - norm_factors
+            params_dict['ps'] = np.exp(log_ps)
         else:
-            params_dict['log_lambdas'] = params
+            params_dict['lambdas'] = np.exp(params)
         return(params_dict) 
 
 class FullKernelAligner(object):
@@ -142,12 +168,18 @@ class FullKernelAligner(object):
         self.n = y.shape[0]
         
         self.kernel.set_data(X, alleles=alleles)
-        y_res = (y - y.mean()).reshape((self.n, 1))
+        y_res = y.reshape((self.n, 1))
         self.target = y_res.dot(y_res.T)
     
-    def squared_frobenius_norm(self, lambdas):
-        cov = self.predict(lambdas=lambdas)
+    def squared_frobenius_norm(self, **kwargs):
+        cov = self.predict(**kwargs)
         return((np.power(cov - self.target,  2).mean()))
+    
+    def squared_frobenius_norm_grad(self, **kwargs):
+        cov = self.predict(**kwargs)
+        diff2 = 2*(cov - self.target)
+        grad = [np.sum(diff2 * grad_k for grad_k in self.kernel.grad())]
+        return(grad)
     
     def calc_reg_lambdas(self, log_lambdas):
         if self.beta == 0:
@@ -159,27 +191,27 @@ class FullKernelAligner(object):
     def calc_reg_ps(self, ps):
         return(0)
     
-    def split_params(self, params):
-        return(params, None)
-    
     def loss(self, params):
-        log_lambdas, ps = self.kernel.split_params(params)
-        frob = self.squared_frobenius_norm(np.exp(log_lambdas))
-        reg_lambdas = self.calc_reg_lambdas(log_lambdas)
-        reg_ps = self.calc_reg_ps(ps)
-        return(frob + reg_lambdas + reg_ps)
+        params_dict = self.kernel.split_params(params)
+        frob = self.squared_frobenius_norm(**params_dict)
+        
+        # reg_lambdas = self.calc_reg_lambdas(params_dict['log_lambdas'])
+        # reg_ps = self.calc_reg_ps(params_dict['log_ps'])
+        
+        
+        return(frob)
     
-    def fit(self, log_lambda0=None):
-        if log_lambda0 is None:
-            log_lambda0 = np.append([-10], 2-np.arange(self.seq_length))
+    def fit(self, params0=None):
+        if params0 is None:
+            params0 = self.kernel.get_params0()
         res = minimize(fun=self.loss,
-                       x0=log_lambda0, method='Powell',
+                       x0=params0, method='Powell',
                        options={'xtol': 1e-8, 'ftol': 1e-8, 'maxiter': 1e5})
         lambdas = np.exp(res.x)
         return(lambdas)
     
-    def predict(self, lambdas):
-        return(self.kernel(lambdas=lambdas))
+    def predict(self, **kwargs):
+        return(self.kernel(**kwargs))
 
 
 class KernelAligner(object):

gpmap/src/linop.py
@@ -32,8 +32,8 @@ class LaplacianOperator(SeqLinOperator):
         super().__init__(n_alleles=n_alleles, seq_length=seq_length)
         self.max_size = max_size
     
-        self.ps = ps
-        self.calc_Kns(ps=ps)
+        self.set_ps(ps)
+        self.calc_Kns()
             
         if max_size is None:
             self.calc_L()
@@ -49,12 +49,12 @@ class LaplacianOperator(SeqLinOperator):
     def set_ps(self, ps):
         if ps is None:
             ps = [np.ones(self.alpha)] * self.l
-        check_error(len(ps) == self.l)
+        check_error(len(ps) == self.l, msg='Number of ps should be equal to length')
         self.ps = ps
     
     def calc_lambdas(self):
         self.lambdas = np.arange(self.l + 1)
-        if self.ps is None:
+        if self.ps is None or np.unique(self.ps).shape[0] == 1:
             self.lambdas *= self.alpha
     
     def calc_Kn(self, p):
@@ -125,7 +125,7 @@ class LaplacianOperator(SeqLinOperator):
                                      for k in range(self.lp1)]
     
     def calc_w(self, k, d):
-        if self.ps is not None:
+        if self.ps is not None and np.unique(self.ps).shape[0] > 1:
             raise ValueError('w method not implemented for unequal ps')
         
         """return value of the Krawtchouk polynomial for k, d"""
@@ -148,7 +148,7 @@ class LapDepOperator(SeqLinOperator):
         if L is None:
             msg = 'either L or both seq_length and n_alleles must be given'
             check_error(n_alleles is not None and seq_length is not None, msg=msg)
-            L = LaplacianOperator(n_alleles, seq_length, max_L_size=max_L_size)
+            L = LaplacianOperator(n_alleles, seq_length, max_size=max_L_size)
         else:
             msg = 'either L or both seq_length and n_alleles must be given'
             check_error(L is not None, msg=msg)
@@ -163,13 +163,17 @@ class DeltaPOperator(LapDepOperator):
         super().__init__(n_alleles=n_alleles, seq_length=seq_length, L=L,
                          max_L_size=max_L_size)
         self.set_P(P)
-        self.n_p_faces = self.calc_n_p_faces()
+        self.calc_kernel_dimension()
+        self.calc_n_p_faces()
+
+    def calc_kernel_dimension(self):
+        self.kernel_dimension = np.sum(self.L.lambdas_multiplicity[:self.P])
     
     def calc_n_p_faces(self):
         n_p_sites = comb(self.l, self.P)
         n_p_faces_per_sites = comb(self.alpha, 2) ** self.P
         allelic_comb_remaining_sites = self.alpha ** (self.l - self.P)
-        return(n_p_sites * n_p_faces_per_sites * allelic_comb_remaining_sites)
+        self.n_p_faces = n_p_sites * n_p_faces_per_sites * allelic_comb_remaining_sites
         
     def set_P(self, P):
         self.P = P
@@ -238,13 +242,13 @@ class VjProjectionOperator(LapDepOperator):
 
     
 class ProjectionOperator(LapDepOperator):
-    def __init__(self, n_alleles=None, seq_length=None, L=None, max_L_size=False):
+    def __init__(self, n_alleles=None, seq_length=None, L=None, max_L_size=None):
         super().__init__(n_alleles=n_alleles, seq_length=seq_length, L=L,
                          max_L_size=max_L_size)
         self.calc_polynomial_coeffs()
     
     def calc_eig_vandermonde_matrix(self):
-        V = np.vstack([self.L.lambdas ** i for i in range(self.seq_length + 1)]).T
+        V = np.vstack([self.L.lambdas ** i for i in range(self.lp1)]).T
         return(V)
     
     def calc_polynomial_coeffs(self, numeric=False):

gpmap/test/test_kernel.py
@@ -104,7 +104,6 @@ class KernelTest(unittest.TestCase):
         # Align with beta = 0
         lambdas_star = aligner.fit()
         pred = aligner.predict(lambdas_star)
-        print(cov - pred)
         assert(np.allclose(cov, pred, rtol=0.01))
         assert(np.allclose(lambdas, lambdas_star, rtol=0.5))
         
@@ -122,31 +121,53 @@ class KernelTest(unittest.TestCase):
         n = [819, 9834, 58980, 176754, 265160, 159214]
         aligner.set_data(cov, n)
         lambdas = aligner.fit()
-        print(lambdas)
         assert(np.all(np.isnan(lambdas) == False))
     
-    def test_full_kernel_alignment(self):
-        np.random.seed(1)
-        seq_length, n_alleles = 4, 4
-        lambdas = np.array([1, 200, 20, 2, 0.2])
+    def test_full_kernel_alignment_small(self):
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        alleles = ['A', 'B']
+        
+        kernel = VarianceComponentKernel(2, 2)
+        aligner = FullKernelAligner(kernel=kernel)
+        
+        # Constant model
+        y = np.array([1, 1, 1, 1])
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        lambdas_star = aligner.fit()
+        assert(np.allclose(lambdas_star, [1, 0, 0]))
+        
+        # Additive model
+        y = np.array([-2, 0, 0, 2])
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        lambdas_star = aligner.fit()
+        assert(np.allclose(lambdas_star, [0, 1, 0]))
+        
+        # Pairwise model
+        y = np.array([1, -1, -1, 1])
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        lambdas_star = aligner.fit()
+        assert(np.allclose(lambdas_star, [0, 0, 1]))
+        
+    def test_full_kernel_alignment_medium(self):
+        l, a = 4, 4
+        lambdas = np.zeros(l+1)
+        lambdas[1] = 1
         
         vc = VCregression()
-        vc.init(seq_length, n_alleles)
+        vc.init(l, a)
         data = vc.simulate(lambdas)
+        X, y = data.index.values, data.y.values
         alleles = np.unique(vc.alphabet)
         
-        kernel = VarianceComponentKernel(seq_length, n_alleles, use_p=False)
+        kernel = VarianceComponentKernel(l, a)
         aligner = FullKernelAligner(kernel=kernel)
-        aligner.set_data(X=data.index.values, y=data.y.values, alleles=alleles)
         
-        # Align with beta = 0
-        lambdas_star = aligner.fit(np.log(lambdas+1e-6))
-        error1 = aligner.squared_frobenius_norm(lambdas_star)
-        error2 = aligner.squared_frobenius_norm(lambdas)
-        assert(error2 >= error1)
-        print(lambdas_star, lambdas)
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        lambdas_star = aligner.fit()
+        assert(np.allclose(lambdas_star / lambdas_star[1], lambdas))
+        
         
         
 if __name__ == '__main__':
-    import sys;sys.argv = ['', 'KernelTest.test_full_kernel_alignment']
+    import sys;sys.argv = ['', 'KernelTest.test_full_kernel_alignment_medium']
     unittest.main()