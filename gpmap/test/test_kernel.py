be33ca91fc23f724f77b96c3782611643fb4b12a
Carlos Marti <carlos199m1@gmail.com>
2023-02-06 10:11:50 -0500

    test for skewed kernel sparse L mode implemented

--- gpmap/test/test_kernel.py
+++ gpmap/test/test_kernel.py
@@ -86,6 +86,47 @@ class KernelTest(unittest.TestCase):
                         [-1.,  1.,  1., -1.],
                         [ 1., -1., -1.,  1.]])
         assert(np.allclose(cov, add))
+        
+    def test_mse(self):
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        kernel = VarianceComponentKernel(2, 2, use_p=False)
+        aligner = FullKernelAligner(kernel=kernel)
+        
+        # Constant function
+        y = np.array([1, 1, 1, 1])
+        lambdas = np.array([1, 0, 0])
+        aligner.set_data(X, y, alleles=['A', 'B'])        
+        mse = aligner.mse(lambdas=lambdas)
+        mse_grad = aligner.mse_grad(lambdas=lambdas)
+        assert(mse == 0)
+        assert(np.allclose(mse_grad, 0))
+        
+        # Additive function
+        y = np.array([-2, 0, 0, 2])
+        lambdas = np.array([0, 1, 0])
+        aligner.set_data(X, y, alleles=['A', 'B'])        
+        mse = aligner.mse(lambdas=lambdas)
+        mse_grad = aligner.mse_grad(lambdas=lambdas)
+        assert(mse == 2)
+        assert(np.allclose(mse_grad, 0))
+        
+        # Pairwise function
+        y = np.array([1, -1, -1, 1])
+        lambdas = np.array([0, 0, 1])
+        aligner.set_data(X, y, alleles=['A', 'B'])        
+        mse = aligner.mse(lambdas=lambdas)
+        mse_grad = aligner.mse_grad(lambdas=lambdas)
+        assert(mse == 0)
+        assert(np.allclose(mse_grad, 0))
+        
+        # Additive function with non zero gradient
+        y = np.array([-1, 0, 0, 1])
+        lambdas = np.array([0, 0.75, 0])
+        aligner.set_data(X, y, alleles=['A', 'B'])        
+        mse = aligner.mse(lambdas=lambdas)
+        mse_grad = aligner.mse_grad(lambdas=lambdas)
+        assert(mse == 0.625)
+        assert(np.allclose(mse_grad, [0, 12, 0]))
     
     def test_kernel_alignment(self):
         np.random.seed(1)
@@ -134,21 +175,22 @@ class KernelTest(unittest.TestCase):
         y = np.array([1, 1, 1, 1])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [1, 0, 0]))
+        assert(np.allclose(lambdas_star, [1, 0, 0], atol=0.01))
         
         # Additive model
         y = np.array([-2, 0, 0, 2])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [0, 1, 0]))
+        assert(np.allclose(lambdas_star, [0, 1, 0], atol=0.01))
         
         # Pairwise model
         y = np.array([1, -1, -1, 1])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [0, 0, 1]))
+        assert(np.allclose(lambdas_star, [0, 0, 1], atol=0.01))
         
     def test_full_kernel_alignment_medium(self):
+        np.random.seed(0)
         l, a = 4, 4
         lambdas = np.zeros(l+1)
         lambdas[1] = 1
@@ -166,8 +208,15 @@ class KernelTest(unittest.TestCase):
         lambdas_star = aligner.fit()
         assert(np.allclose(lambdas_star / lambdas_star[1], lambdas))
         
+        # With known noise
+        y_var = 0.05 * np.ones(y.shape)
+        y = np.random.normal(y, y_var)
+        aligner.set_data(X=X, y=y, y_var=y_var, alleles=alleles)
+        lambdas_star = aligner.fit()
+        assert(np.allclose(lambdas_star / lambdas_star[1], lambdas))
+        
         
         
 if __name__ == '__main__':
-    import sys;sys.argv = ['', 'KernelTest.test_full_kernel_alignment_medium']
+    import sys;sys.argv = ['', 'KernelTest.test_full_kernel_alignment_small']
     unittest.main()
