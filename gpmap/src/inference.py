error when calculating variance components fixed

gpmap/src/inference.py
@@ -267,7 +267,7 @@ class VCregression(LandscapeEstimator):
         return(cov, distance_class_ns)
     
     def lambdas_to_variance(self, lambdas):
-        variance_components = (lambdas * self.W.L.lambdas_multiplicity)[1:]
+        variance_components = (lambdas * self.K.lambdas_multiplicity)[1:]
         variance_components = variance_components / variance_components.sum()
         return(variance_components)
     
@@ -667,6 +667,8 @@ class SeqDEFT(DeltaPEstimator):
         a_min = self.calc_a_min(phi_inf) 
         a_max = self.calc_a_max(phi_inf)
         a_values = np.geomspace(a_min, a_max, self.num_reg)
+        a_values = np.hstack([0, a_values, np.inf])
+        self.total_folds = self.nfolds * (self.num_reg + 2)
         return(a_values)
 
     def fill_zeros_counts(self, X, y):

gpmap/test/test_inference.py
@@ -32,6 +32,13 @@ class VCTests(unittest.TestCase):
         assert(np.all(m[4:, :] == 0))
         assert(m.shape == (8, 4))
     
+    def test_lambdas_to_variance_p(self):
+        vc = VCregression()
+        vc.init(3, 2)
+        lambdas = np.array([0, 1, 0.5, 0.1])
+        v = vc.lambdas_to_variance(lambdas)
+        assert(np.allclose(v, [3/4.6, 1.5/4.6, 0.1/4.6]))
+    
     def test_simulate_vc(self):
         np.random.seed(1)
         sigma = 0.1
@@ -291,5 +298,5 @@ class VCTests(unittest.TestCase):
         
         
 if __name__ == '__main__':
-    import sys;sys.argv = ['', 'VCTests.test_calculate_variance_components']
+    import sys;sys.argv = ['', 'VCTests']
     unittest.main()