0110042ea882804cccb56f8ee79c4fe70b4bf26c
Carlos Marti <carlos199m1@gmail.com>
2023-07-10 16:12:50 -0400

    landscape is not required now as long as tha raw data is available

--- gpmap/src/datasets.py
+++ gpmap/src/datasets.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, exists
 
 from gpmap.src.utils import check_error, read_dataframe
 from gpmap.src.settings import DATASETS, RAW_DATA_DIR, LANDSCAPES_DIR
@@ -12,7 +12,6 @@ class DataSet(object):
         
         self.name = dataset
         self.load_data()
-        self.load_landscape()
     
     def load_data(self):
         fpath = join(RAW_DATA_DIR, '{}.pq'.format(self.name))
@@ -24,9 +23,20 @@ class DataSet(object):
         msg = '"var" column missing from data table'
         check_error('var' in self.data.columns, msg=msg)
     
+    @property
+    def landscape(self):
+        if not hasattr(self, '_landscape'):
+            self.load_landscape()
+        return(self._landscape)
+    
     def load_landscape(self):
         fpath = join(LANDSCAPES_DIR, '{}.pq'.format(self.name))
-        self.landscape = read_dataframe(fpath)
+        
+        if not exists(fpath):
+            msg = 'estimated landscape for dataset {} not found'.format(self.name)
+            raise ValueError(msg)
+        
+        self._landscape = read_dataframe(fpath)
     
     def to_sequence_space(self):
         space = SequenceSpace(X=self.landscape.index.values,
