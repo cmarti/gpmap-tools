Full kernel alignment procedure working Error in constant for connectedness and vc kernels fixed gradient functions fixed for more efficient optimization methods new tests implemented

gpmap/src/inference.py
@@ -21,9 +21,8 @@ from gpmap.src.kernel import KernelAligner
 
 
 class LandscapeEstimator(object):
-    def __init__(self, expand_alphabet=True, max_L_size=None):
+    def __init__(self, expand_alphabet=True):
         self.expand_alphabet = expand_alphabet
-        self.max_L_size = max_L_size
     
     def get_regularization_constants(self):
         return(10**(np.linspace(self.min_log_reg, self.max_log_reg, self.num_reg)))
@@ -64,6 +63,84 @@ class LandscapeEstimator(object):
         self.genotypes = np.array(list(get_seqs_from_alleles(alphabet)))
         self.genotype_idxs = pd.Series(np.arange(self.n_genotypes),
                                        index=self.genotypes)
+        
+
+class GaussianProcessRegressor(object):
+    def __init__(self, kernel, progress=False):
+        self.K = kernel
+        self.n = kernel.n
+        self.progress = progress
+    
+    def predict(self):
+        self.K.set_mode(all_rows=False, add_y_var_diag=True, full_v=False)
+        a_star = self.K.inv_dot(self.y)
+        
+        self.K.set_mode(all_rows=True, add_y_var_diag=False)
+        y_pred = self.K.dot(a_star)
+        return(y_pred)
+    
+    def calc_posterior_variance_i(self, i):
+        self.K.set_mode(full_v=True, all_rows=True)
+        K_i = self.K.get_column(i)
+        K_ii = K_i[i]
+        K_Bi = self.K.gt2data.T.dot(K_i)
+        
+        self.K.set_mode(all_rows=False, add_y_var_diag=True, full_v=False)
+        post_var = K_ii - self.K.inv_quad(K_Bi)
+        return(post_var)
+    
+    def calc_posterior_variance(self, X_pred=None):
+        pred_idx = np.arange(self.n) if X_pred is None else self.get_obs_idx(X_pred)
+        if self.progress:
+            pred_idx = tqdm(pred_idx, total=X_pred.shape[0])
+
+        post_vars = np.array([self.calc_posterior_variance_i(i) for i in pred_idx])        
+        return(post_vars)
+    
+    def sample(self):
+        a = np.random.normal(size=self.n)
+        self.K.set_mode(full_v=True, all_rows=True)
+        y = self.K.transform(a)
+        return(y)
+    
+    def simulate(self, sigma=0):
+        yhat = self.sample()
+        y = np.random.normal(yhat, sigma) if sigma > 0 else yhat
+        y_var = np.full(self.n, sigma**2, dtype=float)
+        return(yhat, y, y_var)
+    
+
+class VCregression(LandscapeEstimator):
+    '''
+        Variance Component regression model that allows inference and prediction
+        of a scalar function in sequence spaces under a Gaussian Process prior
+        parametrized by the contribution of the different orders of interaction
+        to the observed genetic variability of a continuous phenotype
+        
+        It requires the use of the same number of alleles per sites
+            
+    '''
+    def __init__(self, beta=0, cross_validation=False, 
+                 num_beta=20, nfolds=5, min_log_beta=-2,
+                 max_log_beta=7, cv_loss_function='frobenius_norm'):
+        super().__init__()
+        self.beta = beta
+        self.nfolds = nfolds
+        self.num_reg = num_beta
+        self.total_folds = self.nfolds * self.num_reg
+        
+        self.min_log_reg = min_log_beta
+        self.max_log_reg = max_log_beta
+        self.run_cv = cross_validation
+        self.cv_loss_function = cv_loss_function
+        
+    def init(self, seq_length=None, n_alleles=None, genotypes=None,
+             alphabet_type='custom'):
+        self.define_space(seq_length=seq_length, n_alleles=n_alleles,
+                          genotypes=genotypes, alphabet_type=alphabet_type)
+        self.kernel_aligner = KernelAligner(self.seq_length, self.n_alleles)
+        self.K = VarianceComponentKernelOperator(self.n_alleles, self.seq_length)
+        self.calc_L_powers_unique_entries_matrix() # For covariance d calculations
     
     def calc_L_powers_unique_entries_matrix(self):
         """Construct entries of powers of L. 
@@ -102,40 +179,7 @@ class LandscapeEstimator(object):
         # Invert MAT
         self.L_powers_unique_entries_inv = np.linalg.inv(MAT)
         self.L_powers_unique_entries = MAT
-        
-
-class VCregression(LandscapeEstimator):
-    '''
-        Variance Component regression model that allows inference and prediction
-        of a scalar function in sequence spaces under a Gaussian Process prior
-        parametrized by the contribution of the different orders of interaction
-        to the observed genetic variability of a continuous phenotype
-        
-        It requires the use of the same number of alleles per sites
-            
-    '''
-    def __init__(self, beta=0, cross_validation=False, 
-                 num_beta=20, nfolds=5, min_log_beta=-2,
-                 max_log_beta=7, cv_loss_function='frobenius_norm'):
-        super().__init__()
-        self.beta = beta
-        self.nfolds = nfolds
-        self.num_reg = num_beta
-        self.total_folds = self.nfolds * self.num_reg
-        
-        self.min_log_reg = min_log_beta
-        self.max_log_reg = max_log_beta
-        self.run_cv = cross_validation
-        self.cv_loss_function = cv_loss_function
-        
-    def init(self, seq_length=None, n_alleles=None, genotypes=None,
-             alphabet_type='custom'):
-        self.define_space(seq_length=seq_length, n_alleles=n_alleles,
-                          genotypes=genotypes, alphabet_type=alphabet_type)
-        self.kernel_aligner = KernelAligner(self.seq_length, self.n_alleles)
-        self.K = VarianceComponentKernelOperator(self.n_alleles, self.seq_length)
-        self.calc_L_powers_unique_entries_matrix() # For covariance d calculations
-        
+    
     def get_obs_idx(self, seqs):
         obs_idx = self.genotype_idxs[seqs]
         return(obs_idx)

gpmap/src/kernel.py
@@ -15,18 +15,26 @@ class SequenceKernel(object):
         self.alpha = n_alleles
         self.l = seq_length
         self.lp1 = self.l + 1
+        self.n = self.alpha ** self.l
 
     def calc_hamming_distance(self, x1, x2):
         return(self.l - inner_product(x1, x2))
     
+    def get_hamming_distance(self):
+        if self._hamming is None: # this happens when data is re-set
+            self._hamming = self.calc_hamming_distance(self.x1, self.x2)
+        return(self._hamming)
+    
     def set_data(self, x1, x2=None, alleles=None, **kwargs):
         self.x1 = seq_to_one_hot(x1, alleles=alleles).astype(int)
         if x2 is None:
-            x2 = x1
-        self.x2 = seq_to_one_hot(x2, alleles=alleles).astype(int)
+            self.x2 = self.x1
+        else:
+            self.x2 = seq_to_one_hot(x2, alleles=alleles).astype(int)
         
         if hasattr(self, 'set_extra_data'):
             self.set_extra_data(**kwargs)
+        self._hamming = None
     
     def __call__(self, **kwargs):
         return(self.forward(**kwargs))
@@ -48,7 +56,7 @@ class VarianceComponentKernel(SequenceKernel):
         ss = 0
         for q in range(self.lp1):
             ss += (-1.)**q * (self.alpha-1.)**(k-q) * comb(d,q) * comb(self.l-d,k-q)
-        return(ss)
+        return(ss / self.n)
     
     def calc_krawchouk_matrix(self):
         w_kd = np.zeros((self.lp1, self.lp1))
@@ -56,23 +64,26 @@ class VarianceComponentKernel(SequenceKernel):
             for d in range(self.lp1):
                 w_kd[d, k] = self.calc_w_kd(k, d)
         self.W_kd = w_kd
-    
+        
     def forward(self, lambdas):
         hamming_distance = self.calc_hamming_distance(self.x1, self.x2)
         cov = self.W_kd.dot(lambdas)[hamming_distance]
         return(cov)
     
     def backward(self, lambdas):
-        hamming_distance = self.calc_hamming_distance(self.x1, self.x2)
+        hamming_distance = self.get_hamming_distance()
         for k, lambda_k in enumerate(lambdas):
-            yield(lambda_k * self.W_kd[k, :][hamming_distance])
+            yield(self.transform_params_grad(lambda_k) * self.W_kd[:, k][hamming_distance])
 
     def get_params0(self):
-        params = np.append([-10], 2-np.arange(self.l))
+        params = np.append([-5], 2-np.arange(self.l))
         return(params)
     
     def transform_params(self, params):
         return(np.exp(params))
+    
+    def transform_params_grad(self, f_params):
+        return(f_params)
     
     def split_params(self, params):
         params_dict = {'lambdas': self.transform_params(params)}
@@ -80,9 +91,10 @@ class VarianceComponentKernel(SequenceKernel):
 
 
 class ConnectednessKernel(SequenceKernel):
-    def __init__(self, seq_length, n_alleles):
+    def __init__(self, seq_length, n_alleles, sites_equal=False):
         super().__init__(seq_length=seq_length, n_alleles=n_alleles)
         self.n_params = self.lp1
+        self.sites_equal = sites_equal
     
     def set_extra_data(self):
         self.same_seq = self.x1.dot(self.x2.T) == self.l
@@ -103,30 +115,212 @@ class ConnectednessKernel(SequenceKernel):
         values = np.hstack([[self.calc_factor(r)] * self.alpha for r in rho])
         m = get_sparse_diag_matrix(values)
         return(m)
-        
+    
     def forward(self, rho):
         rho = self.get_rho(rho)
-        metric = self.calc_metric(rho)
-        c = np.prod(1-rho)
-        cov = c * np.exp(inner_product(self.x1, self.x2, metric=metric))
-        return(cov)
+        if self.sites_equal:
+            d = self.get_hamming_distance()
+            s = np.log((1 + (self.alpha -1) * rho[0]))
+            cov = np.exp(d * (np.log(1 - rho[0]) - s) + self.l * s - np.log(self.n))
+        else:
+            metric = self.calc_metric(rho)
+            c = np.prod(1-rho)
+            cov = c * np.exp(inner_product(self.x1, self.x2, metric=metric) - np.log(self.n))
+            
+        self.cov = cov 
+        self.rho = rho
+        return(self.cov)
     
-    def backward(self):
-        hamming_distance = self.calc_hamming_distance(self.x1, self.x2)
-        for w_k in self.W_kd:
-            yield(w_k[hamming_distance])
+    def backward(self, **kwargs):
+        rho = self.rho
+        if self.sites_equal:
+            rho = rho[0]
+            d = self.get_hamming_distance()
+            n = (self.alpha - 1) / (1 + (self.alpha -1) * rho) * self.l
+            m = -self.alpha / ((1 - rho) * (1 + (self.alpha-1) * rho))
+            factor = m * d + n
+            yield(factor * self.cov * self.transform_params_grad(rho))
+        
+        else:
+            for p, rho_p in enumerate(rho):
+                idx = np.arange(p * self.alpha, (p + 1) * self.alpha)
+                s = inner_product(self.x1[:, idx], self.x2[:, idx])
+                different_factor = -1 / (1-rho_p)
+                equal_factor = (self.alpha - 1) / (1 + (self.alpha - 1) * rho_p)
+                factor = equal_factor * s + (1 - s) * different_factor
+                yield(factor * self.cov * self.transform_params_grad(rho_p))
 
     def get_params0(self):
-        params = np.random.normal(size=self.l)
+        s = 1 if self.sites_equal else self.l
+        params = np.random.normal(size=s)
         return(params)
     
     def transform_params(self, params):
         return(np.exp(params) / (1 + np.exp(params)))
     
+    def inv_transform_params(self, params):
+        return(np.log(params / (1 - params)))
+    
+    def transform_params_grad(self, f_params):
+        params = self.inv_transform_params(f_params)
+        return(np.exp(params) / (1 + np.exp(params)) ** 2)
+    
     def split_params(self, params):
         params_dict = {'rho': self.transform_params(params)}
         return(params_dict)
 
+
+class FullKernelAligner(object):
+    def __init__(self, kernel, optimizer='BFGS'):
+        self.kernel = kernel
+        self.seq_length = kernel.l
+        self.n_alleles = kernel.alpha
+        self.optimizer = optimizer
+    
+    def set_data(self, X, y, y_var=None, alleles=None):
+        self.X = X
+        self.y = y
+        self.y_var = y_var if y_var is not None else np.zeros(y.shape)
+        self.n = y.shape[0]
+        
+        self.kernel.set_data(X, alleles=alleles)
+        y_res = y.reshape((self.n, 1))
+        self.target = y_res.dot(y_res.T)
+    
+    def frob2(self, **kwargs):
+        cov = self.predict(**kwargs) + np.diag(self.y_var)
+        self.residuals = cov - self.target
+        return(np.power(self.residuals,  2).sum())
+    
+    def loss(self, params):
+        params_dict = self.kernel.split_params(params)
+        frob = self.frob2(**params_dict)
+        return(frob)
+    
+    def frob2_grad(self, **kwargs):
+        grad = np.array([np.sum(2 * self.residuals * grad_k)
+                         for grad_k in self.kernel.grad(**kwargs)])
+        return(grad)
+    
+    def loss_grad(self, params):
+        params_dict = self.kernel.split_params(params)
+        grad = self.frob2_grad(**params_dict)
+        return(grad)
+    
+    def fit(self, params0=None):
+        if params0 is None:
+            params0 = self.kernel.get_params0()
+        jac = self.grad = None if self.optimizer.lower() == 'powell' else self.loss_grad
+        res = minimize(fun=self.loss, jac=jac, x0=params0,
+                       method=self.optimizer,
+                       options={'gtol': 1e-12, 'maxiter': 1e5})
+        self.res = res
+        params = self.kernel.transform_params(res.x)
+        return(params)
+    
+    def predict(self, **kwargs):
+        return(self.kernel(**kwargs))
+
+
+class KernelAligner(object):
+    def __init__(self, seq_length, n_alleles, beta=0):
+        self.seq_length = seq_length
+        self.n_alleles = n_alleles
+        self.set_beta(beta)
+        self.calc_W_kd_matrix()
+        self.calc_second_order_diff_matrix()
+    
+    def set_data(self, covs, distances_n):
+        self.covs = covs
+        self.distances_n = distances_n
+        self.construct_a(covs, distances_n)
+        self.construct_M(distances_n)
+        self.M_inv = np.linalg.inv(self.M)
+    
+    def set_beta(self, beta):
+        check_error(beta >=0, msg='beta must be >= 0')
+        self.beta = beta
+    
+    def calc_second_order_diff_matrix(self):
+        """Construct second order difference matrix for regularization"""
+        Diff2 = np.zeros((self.seq_length - 2, self.seq_length))
+        for i in range(Diff2.shape[0]):
+            Diff2[i, i:i + 3] = [-1, 2, -1]
+        self.second_order_diff_matrix = Diff2.T.dot(Diff2)
+    
+    def frobenius_norm(self, log_lambdas):
+        """cost function for regularized least square method for inferring 
+        lambdas"""
+        lambdas = np.exp(log_lambdas)
+        Frob1 = lambdas.dot(self.M).dot(lambdas)
+        Frob2 = 2 * lambdas.dot(self.a)
+        Frob = Frob1 - Frob2
+        if self.beta > 0:
+            Frob += self.beta * quad(self.second_order_diff_matrix, log_lambdas[1:])
+        return(Frob)
+    
+    def frobenius_norm_grad(self, log_lambdas):
+        msg = 'gradient calculation only implemented for beta=0'
+        check_error(self.beta == 0, msg=msg)
+        lambdas = np.exp(log_lambdas)
+        grad_Frob = (2 * self.M.dot(lambdas) - 2 * self.a)
+        return(grad_Frob * lambdas)
+    
+    def construct_M(self, N_d):
+        size = self.seq_length + 1
+        M = np.zeros([size, size])
+        for i in range(size):
+            for j in range(size):
+                for d in range(size):
+                    M[i, j] += N_d[d] * self.W_kd[i, d] * self.W_kd[j, d]
+        self.M = M
+    
+    def construct_a(self, rho_d, N_d):
+        size = self.seq_length + 1
+        a = np.zeros(size)
+        for i in range(size):
+            for d in range(size):
+                a[i] += N_d[d] * self.W_kd[i, d] * rho_d[d]
+        self.a = a
+    
+    def calc_w(self, k, d):
+        """return value of the Krawtchouk polynomial for k, d"""
+        l, a = self.seq_length, self.n_alleles
+        s = 0
+        for q in range(l + 1):
+            s += (-1)**q * (a - 1)**(k - q) * comb(d, q) * comb(l - d, k - q)
+        return(s / a**l)
+    
+    def calc_W_kd_matrix(self):
+        """return full matrix l+1 by l+1 Krawtchouk matrix"""
+        self.W_kd = np.zeros([self.seq_length + 1, self.seq_length + 1])
+        for k in range(self.seq_length + 1):
+            for d in range(self.seq_length + 1):
+                self.W_kd[k, d] = self.calc_w(k, d)
+    
+    def fit(self):
+        lambdas0 = np.dot(self.M_inv, self.a).flatten()
+        lambdas0[lambdas0<0] = 1e-10
+        log_lambda0 = np.log(lambdas0)
+        if self.beta == 0:
+            res = minimize(fun=self.frobenius_norm,
+                           jac=self.frobenius_norm_grad,
+                           x0=log_lambda0, method='L-BFGS-B')
+        elif self.beta > 0:
+            res = minimize(fun=self.frobenius_norm,
+                           x0=log_lambda0, method='Powell',
+                           options={'xtol': 1e-8, 'ftol': 1e-8})
+        lambdas = np.exp(res.x)
+        return(lambdas)
+    
+    def predict(self, lambdas):
+        return(self.W_kd.T.dot(lambdas))
+    
+    def calc_mse(self, lambdas):
+        ss = (self.predict(lambdas) - self.covs) ** 2
+        return(np.sum(ss * (1 / np.sum(self.distances_n)) * self.distances_n))
+
+###### Skewed Kernel ######
 
 class SkewedVarianceComponentKernel(SequenceKernel):
     def __init__(self, seq_length, n_alleles, q=None, use_p=False):
@@ -237,158 +431,3 @@ class SkewedVarianceComponentKernel(SequenceKernel):
         else:
             params_dict['lambdas'] = np.exp(params)
         return(params_dict) 
-
- 
-class FullKernelAligner(object):
-    def __init__(self, kernel, optimizer='powell'):
-        self.kernel = kernel
-        self.seq_length = kernel.l
-        self.n_alleles = kernel.alpha
-        self.optimizer = optimizer
-    
-    def set_data(self, X, y, y_var=None, alleles=None):
-        self.X = X
-        self.y = y
-        self.y_var = y_var if y_var is not None else np.zeros(y.shape)
-        self.n = y.shape[0]
-        
-        self.kernel.set_data(X, alleles=alleles)
-        y_res = y.reshape((self.n, 1))
-        self.target = y_res.dot(y_res.T) - np.diag(self.y_var)
-    
-    def _mse(self, **kwargs):
-        cov = self.predict(**kwargs)
-        mse = (np.power(cov - self.target,  2).mean())
-        return(mse)
-    
-    def _mse_grad(self, **kwargs):
-        cov = self.predict(**kwargs)
-        diff2 = 2*(cov - self.target)
-        grad = np.array([np.sum(diff2 * grad_k) for grad_k in self.kernel.grad(**kwargs)])
-        return(grad)
-    
-    def loss(self, params):
-        params_dict = self.kernel.split_params(params)
-        frob = self._mse(**params_dict)
-        return(frob)
-    
-    def grad(self, params):
-        params_dict = self.kernel.split_params(params)
-        grad = self._mse_grad(**params_dict)
-        return(grad)
-    
-    def fit(self, params0=None):
-        if params0 is None:
-            params0 = self.kernel.get_params0()
-        jac = self.grad = None if self.optimizer == 'powell' else self.grad
-        res = minimize(fun=self.loss, jac=jac,
-                       x0=params0,
-                       method=self.optimizer,
-#                        options={'ftol': 1e-4, 'maxiter': 1e5}
-                       ) # 'xtol': 1e-8, 
-        self.res = res
-        params = self.kernel.transform_params(res.x)
-        return(params)
-    
-    def predict(self, **kwargs):
-        return(self.kernel(**kwargs))
-
-
-class KernelAligner(object):
-    def __init__(self, seq_length, n_alleles, beta=0):
-        self.seq_length = seq_length
-        self.n_alleles = n_alleles
-        self.set_beta(beta)
-        self.calc_W_kd_matrix()
-        self.calc_second_order_diff_matrix()
-    
-    def set_data(self, covs, distances_n):
-        self.covs = covs
-        self.distances_n = distances_n
-        self.construct_a(covs, distances_n)
-        self.construct_M(distances_n)
-        self.M_inv = np.linalg.inv(self.M)
-    
-    def set_beta(self, beta):
-        check_error(beta >=0, msg='beta must be >= 0')
-        self.beta = beta
-    
-    def calc_second_order_diff_matrix(self):
-        """Construct second order difference matrix for regularization"""
-        Diff2 = np.zeros((self.seq_length - 2, self.seq_length))
-        for i in range(Diff2.shape[0]):
-            Diff2[i, i:i + 3] = [-1, 2, -1]
-        self.second_order_diff_matrix = Diff2.T.dot(Diff2)
-    
-    def frobenius_norm(self, log_lambdas):
-        """cost function for regularized least square method for inferring 
-        lambdas"""
-        lambdas = np.exp(log_lambdas)
-        Frob1 = lambdas.dot(self.M).dot(lambdas)
-        Frob2 = 2 * lambdas.dot(self.a)
-        Frob = Frob1 - Frob2
-        if self.beta > 0:
-            Frob += self.beta * quad(self.second_order_diff_matrix, log_lambdas[1:])
-        return(Frob)
-    
-    def frobenius_norm_grad(self, log_lambdas):
-        msg = 'gradient calculation only implemented for beta=0'
-        check_error(self.beta == 0, msg=msg)
-        lambdas = np.exp(log_lambdas)
-        grad_Frob = (2 * self.M.dot(lambdas) - 2 * self.a)
-        return(grad_Frob * lambdas)
-    
-    def construct_M(self, N_d):
-        size = self.seq_length + 1
-        M = np.zeros([size, size])
-        for i in range(size):
-            for j in range(size):
-                for d in range(size):
-                    M[i, j] += N_d[d] * self.W_kd[i, d] * self.W_kd[j, d]
-        self.M = M
-    
-    def construct_a(self, rho_d, N_d):
-        size = self.seq_length + 1
-        a = np.zeros(size)
-        for i in range(size):
-            for d in range(size):
-                a[i] += N_d[d] * self.W_kd[i, d] * rho_d[d]
-        self.a = a
-    
-    def calc_w(self, k, d):
-        """return value of the Krawtchouk polynomial for k, d"""
-        l, a = self.seq_length, self.n_alleles
-        s = 0
-        for q in range(l + 1):
-            s += (-1)**q * (a - 1)**(k - q) * comb(d, q) * comb(l - d, k - q)
-        return(s / a**l)
-    
-    def calc_W_kd_matrix(self):
-        """return full matrix l+1 by l+1 Krawtchouk matrix"""
-        self.W_kd = np.zeros([self.seq_length + 1, self.seq_length + 1])
-        for k in range(self.seq_length + 1):
-            for d in range(self.seq_length + 1):
-                self.W_kd[k, d] = self.calc_w(k, d)
-    
-    def fit(self):
-        lambdas0 = np.dot(self.M_inv, self.a).flatten()
-        lambdas0[lambdas0<0] = 1e-10
-        log_lambda0 = np.log(lambdas0)
-        if self.beta == 0:
-            res = minimize(fun=self.frobenius_norm,
-                           jac=self.frobenius_norm_grad,
-                           x0=log_lambda0, method='L-BFGS-B')
-        elif self.beta > 0:
-            res = minimize(fun=self.frobenius_norm,
-                           x0=log_lambda0, method='Powell',
-                           options={'xtol': 1e-8, 'ftol': 1e-8})
-        lambdas = np.exp(res.x)
-        return(lambdas)
-    
-    def predict(self, lambdas):
-        return(self.W_kd.T.dot(lambdas))
-    
-    def calc_mse(self, lambdas):
-        ss = (self.predict(lambdas) - self.covs) ** 2
-        return(np.sum(ss * (1 / np.sum(self.distances_n)) * self.distances_n))
-              

gpmap/test/test_kernel.py
@@ -4,10 +4,13 @@ import unittest
 import numpy as np
 
 from scipy.special._basic import comb
-from gpmap.src.inference import VCregression
+from gpmap.src.inference import VCregression, GaussianProcessRegressor
 from gpmap.src.kernel import (VarianceComponentKernel, SequenceKernel,
                               KernelAligner, FullKernelAligner,
-    SkewedVarianceComponentKernel, ConnectednessKernel)
+                              SkewedVarianceComponentKernel, ConnectednessKernel)
+from gpmap.src.linop import ConnectednessKernelOperator,\
+    VarianceComponentKernelOperator
+from gpmap.src.seq import generate_possible_sequences
 
 
 class KernelTest(unittest.TestCase):
@@ -40,7 +43,7 @@ class KernelTest(unittest.TestCase):
         # Test constant covariance
         lambdas = np.array([1, 0, 0])
         cov = kernel(lambdas=lambdas)
-        assert(np.allclose(cov, 1))
+        assert(np.allclose(cov, 1/4))
         
         # Test additive covariance
         lambdas = np.array([0, 1, 0])
@@ -49,94 +52,92 @@ class KernelTest(unittest.TestCase):
                         [ 0.,  2., -2.,  0.],
                         [ 0., -2.,  2.,  0.],
                         [-2.,  0.,  0.,  2.]])
-        assert(np.allclose(cov, add))
+        assert(np.allclose(cov, 1/4 * add))
         
         # Test 2nd order covariance
         lambdas = np.array([0, 0, 1])
         cov = kernel(lambdas=lambdas)
-        add = np.array([[ 1., -1., -1.,  1.],
-                        [-1.,  1.,  1., -1.],
-                        [-1.,  1.,  1., -1.],
-                        [ 1., -1., -1.,  1.]])
-        assert(np.allclose(cov, add))
+        pair = np.array([[ 1., -1., -1.,  1.],
+                         [-1.,  1.,  1., -1.],
+                         [-1.,  1.,  1., -1.],
+                         [ 1., -1., -1.,  1.]])
+        assert(np.allclose(cov, 1/4 * pair))
     
     def test_rho_kernel(self):
         kernel = ConnectednessKernel(2, 2)
         X = np.array(['AA', 'AB', 'BA', 'BB'])
         kernel.set_data(X, alleles=['A', 'B'])
         
+        cov = kernel(rho=[0.5])
+        assert(np.allclose(cov[0], np.array([9/16., 3/16, 3/16, 1/16])))
+        
         cov = kernel(rho=[0.5, 0.2])
-        assert(np.allclose(cov[0], [1.8, 1.2, 0.6, 0.4]))
+        assert(np.allclose(cov[0], 1/4 * np.array([1.8, 1.2, 0.6, 0.4])))
     
-    def test_vc_kernel_ps(self):
-        kernel = SkewedVarianceComponentKernel(2, 2, use_p=True)
+    def test_rho_kernel_grad(self):
         X = np.array(['AA', 'AB', 'BA', 'BB'])
+
+        # Equal sites        
+        kernel = ConnectednessKernel(2, 2, sites_equal=True)
         kernel.set_data(X, alleles=['A', 'B'])
+        kernel(rho=[0.5])
+        grad = list(kernel.backward())[0]
+        assert(np.allclose(grad[0], np.array([0.75, -0.25, -0.25, -0.25]) / 4))
         
-        # Test constant covariance
-        lambdas = np.array([1, 0, 0])
-        log_p = np.log(0.5) * np.ones((2, 2))
-        cov = kernel(lambdas=lambdas, log_p=log_p)
-        assert(np.allclose(cov, 1))
+        # Variable rho across sites
+        kernel = ConnectednessKernel(2, 2, sites_equal=False)
+        kernel.set_data(X, alleles=['A', 'B'])
+        kernel(rho=[0.5, 0.5])
+        grads = list(kernel.backward())
+
+        # Partial derivatives depending on the site that is different        
+        assert(np.allclose(grads[0][0], np.array([ 0.375, 0.125, -0.375,  -0.125]) / 4))
+        assert(np.allclose(grads[1][0], np.array([ 0.375, -0.375,  0.125, -0.125]) / 4))
+
+        # Variable rho method should give the same solution when rho are fixed
+        grad = grads[0] + grads[1]
+        assert(np.allclose(grad[0], np.array([0.75, -0.25, -0.25, -0.25]) / 4))
         
-        # Test additive covariance
-        lambdas = np.array([0, 1, 0])
-        cov = kernel(lambdas=lambdas, log_p=log_p)
-        add = np.array([[ 2.,  0.,  0., -2.],
-                        [ 0.,  2., -2.,  0.],
-                        [ 0., -2.,  2.,  0.],
-                        [-2.,  0.,  0.,  2.]])
-        assert(np.allclose(cov, add))
-        
-        # Test 2nd order covariance
-        lambdas = np.array([0, 0, 1])
-        cov = kernel(lambdas=lambdas, log_p=log_p)
-        add = np.array([[ 1., -1., -1.,  1.],
-                        [-1.,  1.,  1., -1.],
-                        [-1.,  1.,  1., -1.],
-                        [ 1., -1., -1.,  1.]])
-        assert(np.allclose(cov, add))
-        
-    def test_mse(self):
+    def test_frob2(self):
         X = np.array(['AA', 'AB', 'BA', 'BB'])
-        kernel = VarianceComponentKernel(2, 2, use_p=False)
+        kernel = VarianceComponentKernel(2, 2)
         aligner = FullKernelAligner(kernel=kernel)
         
         # Constant function
-        y = np.array([1, 1, 1, 1])
+        y = np.array([1, 1, 1, 1]) / 2
         lambdas = np.array([1, 0, 0])
         aligner.set_data(X, y, alleles=['A', 'B'])        
-        mse = aligner.mse(lambdas=lambdas)
-        mse_grad = aligner.mse_grad(lambdas=lambdas)
-        assert(mse == 0)
-        assert(np.allclose(mse_grad, 0))
+        frob2 = aligner.frob2(lambdas=lambdas)
+        frob2_grad = aligner.frob2_grad(lambdas=lambdas)
+        assert(frob2 == 0)
+        assert(np.allclose(frob2_grad, 0))
         
         # Additive function
-        y = np.array([-2, 0, 0, 2])
+        y = np.array([-2, 0, 0, 2]) / 2
         lambdas = np.array([0, 1, 0])
         aligner.set_data(X, y, alleles=['A', 'B'])        
-        mse = aligner.mse(lambdas=lambdas)
-        mse_grad = aligner.mse_grad(lambdas=lambdas)
-        assert(mse == 2)
-        assert(np.allclose(mse_grad, 0))
+        frob2 = aligner.frob2(lambdas=lambdas)
+        frob2_grad = aligner.frob2_grad(lambdas=lambdas)
+        assert(frob2 == 2)
+        assert(np.allclose(frob2_grad, 0))
         
         # Pairwise function
-        y = np.array([1, -1, -1, 1])
+        y = np.array([1, -1, -1, 1]) / 2
         lambdas = np.array([0, 0, 1])
         aligner.set_data(X, y, alleles=['A', 'B'])        
-        mse = aligner.mse(lambdas=lambdas)
-        mse_grad = aligner.mse_grad(lambdas=lambdas)
-        assert(mse == 0)
-        assert(np.allclose(mse_grad, 0))
+        frob2 = aligner.frob2(lambdas=lambdas)
+        frob2_grad = aligner.frob2_grad(lambdas=lambdas)
+        assert(frob2 == 0)
+        assert(np.allclose(frob2_grad, 0))
         
         # Additive function with non zero gradient
-        y = np.array([-1, 0, 0, 1])
+        y = np.array([-1, 0, 0, 1]) / 2
         lambdas = np.array([0, 0.75, 0])
         aligner.set_data(X, y, alleles=['A', 'B'])        
-        mse = aligner.mse(lambdas=lambdas)
-        mse_grad = aligner.mse_grad(lambdas=lambdas)
-        assert(mse == 0.625)
-        assert(np.allclose(mse_grad, [0, 12, 0]))
+        frob2 = aligner.frob2(lambdas=lambdas)
+        frob2_grad = aligner.frob2_grad(lambdas=lambdas)
+        assert(frob2 == 0.625)
+        assert(np.allclose(frob2_grad, [0, 1.5, 0]))
     
     def test_kernel_alignment(self):
         np.random.seed(1)
@@ -174,7 +175,7 @@ class KernelTest(unittest.TestCase):
         lambdas = aligner.fit()
         assert(np.all(np.isnan(lambdas) == False))
     
-    def test_full_kernel_alignment_small(self):
+    def test_full_kernel_alignment(self):
         X = np.array(['AA', 'AB', 'BA', 'BB'])
         alleles = ['A', 'B']
         
@@ -185,21 +186,47 @@ class KernelTest(unittest.TestCase):
         y = np.array([1, 1, 1, 1])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [1, 0, 0], atol=0.01))
+        assert(np.allclose(lambdas_star, [4, 0, 0], atol=0.01))
         
         # Additive model
         y = np.array([-2, 0, 0, 2])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [0, 1, 0], atol=0.01))
+        assert(np.allclose(lambdas_star, [0, 4, 0], atol=0.01))
         
         # Pairwise model
         y = np.array([1, -1, -1, 1])
         aligner.set_data(X=X, y=y, alleles=alleles)
         lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star, [0, 0, 1], atol=0.01))
+        assert(np.allclose(lambdas_star, [0, 0, 4], atol=0.01))
     
-    def test_full_kernel_alignment_small_rho(self):
+    def test_full_kernel_alignment_comparison(self):
+        np.random.seed(0)
+        l, a = 4, 4
+        alleles = ['A', 'C', 'G', 'T']
+        X = np.array(list(generate_possible_sequences(l, alleles)))
+        
+        # Simulate
+        lambdas0 = 2.**-np.arange(l+1)
+        kernel = VarianceComponentKernelOperator(a, l, lambdas=lambdas0)
+        model = GaussianProcessRegressor(kernel=kernel)
+        y = model.sample()
+
+        # Run collapsed kernel alignment        
+        model = VCregression()
+        model.init(l, a)
+        lambdas_star1 = model.fit(X, y)
+        rel_err = np.abs(np.log2(lambdas_star1 / lambdas0))
+        assert(np.all(rel_err[1:]<0.5))
+        
+        # Run full kernel alignment
+        kernel = VarianceComponentKernel(l, a)
+        aligner = FullKernelAligner(kernel=kernel)
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        lambdas_star2 = aligner.fit()
+        assert(np.allclose(lambdas_star1, lambdas_star2, atol=1e-4))
+        
+    def test_full_kernel_alignment_rho(self):
         X = np.array(['AA', 'AB', 'BA', 'BB'])
         alleles = ['A', 'B']
         
@@ -213,37 +240,79 @@ class KernelTest(unittest.TestCase):
         aligner.target = np.array([[1.8, 1.2, 0.6, 0.4],
                                    [1.2, 1.8, 0.4, 0.6],
                                    [0.6, 0.4, 1.8, 1.2],
-                                   [0.4, 0.6, 1.2, 1.8]])
+                                   [0.4, 0.6, 1.2, 1.8]]) / 4
         
         rho_star = aligner.fit()
         assert(np.allclose(rho_star, [0.5, 0.2]))
         
-    def test_full_kernel_alignment_medium(self):
+    def test_full_kernel_alignment_rho_comparison(self):
         np.random.seed(0)
-        l, a = 4, 4
-        lambdas = np.zeros(l+1)
-        lambdas[1] = 1
+        l, a = 5, 4
+        alleles = ['A', 'C', 'G', 'T']
+        X = np.array(list(generate_possible_sequences(l, alleles)))
         
-        vc = VCregression()
-        vc.init(l, a)
-        data = vc.simulate(lambdas)
-        X, y = data.index.values, data.y.values
-        alleles = np.unique(vc.alphabet)
-        
-        kernel = VarianceComponentKernel(l, a)
+        # Simulate
+        rho0 = 0.5
+        kernel = ConnectednessKernelOperator(a, l, rho=rho0)
+        model = GaussianProcessRegressor(kernel=kernel)
+        y = model.sample()
+
+        # Run kernel alignment        
+        kernel = ConnectednessKernel(l, a, sites_equal=True)
         aligner = FullKernelAligner(kernel=kernel)
-        
         aligner.set_data(X=X, y=y, alleles=alleles)
-        lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star / lambdas_star[1], lambdas))
-        
-        # With known noise
-        y_var = 0.05 * np.ones(y.shape)
-        y = np.random.normal(y, y_var)
-        aligner.set_data(X=X, y=y, y_var=y_var, alleles=alleles)
-        lambdas_star = aligner.fit()
-        assert(np.allclose(lambdas_star / lambdas_star[1], lambdas))
+        rho_star = aligner.fit()[0]
+        assert(np.allclose(rho0, rho_star, atol=0.01))
     
+    def test_full_kernel_alignment_variable_rho(self):
+        np.random.seed(1)
+        rho0 = [0.5, 0.1, 0.7, 0.9]
+        l, a = len(rho0), 4
+        alleles = ['A', 'C', 'G', 'T']
+        X = np.array(list(generate_possible_sequences(l, alleles)))
+        
+        # Simulate
+        kernel = ConnectednessKernelOperator(a, l, rho=rho0)
+        model = GaussianProcessRegressor(kernel=kernel)
+        y = model.sample()
+
+        # Run kernel alignment        
+        kernel = ConnectednessKernel(l, a, sites_equal=False)
+        aligner = FullKernelAligner(kernel=kernel)
+        aligner.set_data(X=X, y=y, alleles=alleles)
+        rho_star = aligner.fit()
+        rel_err = np.abs(np.log2(rho_star / rho0))
+        assert(np.all(rel_err[1:]<0.5))
+    
+    def xtest_vc_kernel_ps(self):
+        kernel = SkewedVarianceComponentKernel(2, 2, use_p=True)
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        kernel.set_data(X, alleles=['A', 'B'])
+        
+        # Test constant covariance
+        lambdas = np.array([1, 0, 0])
+        log_p = np.log(0.5) * np.ones((2, 2))
+        cov = kernel(lambdas=lambdas, log_p=log_p)
+        assert(np.allclose(cov, 1))
+        
+        # Test additive covariance
+        lambdas = np.array([0, 1, 0])
+        cov = kernel(lambdas=lambdas, log_p=log_p)
+        add = np.array([[ 2.,  0.,  0., -2.],
+                        [ 0.,  2., -2.,  0.],
+                        [ 0., -2.,  2.,  0.],
+                        [-2.,  0.,  0.,  2.]])
+        assert(np.allclose(cov, add))
+        
+        # Test 2nd order covariance
+        lambdas = np.array([0, 0, 1])
+        cov = kernel(lambdas=lambdas, log_p=log_p)
+        add = np.array([[ 1., -1., -1.,  1.],
+                        [-1.,  1.,  1., -1.],
+                        [-1.,  1.,  1., -1.],
+                        [ 1., -1., -1.,  1.]])
+        assert(np.allclose(cov, add))
+        
     def xtest_NK_lambdas(self):
         l, a = 7, 4
         P = 3
@@ -254,7 +323,6 @@ class KernelTest(unittest.TestCase):
         print(kernel.W_kd.dot(lambdas))
         cov = np.array([comb(l-d, P) for d in range(l+1)])
         print(cov)
-        
         
         
 if __name__ == '__main__':