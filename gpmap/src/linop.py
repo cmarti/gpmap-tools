489e2ae3af91d8e2268f57d45bc6365d3949ec19
Carlos Marti <carlos199m1@gmail.com>
2023-09-26 18:29:10 -0400

    minor modifications for estimation of log_det methods

--- gpmap/src/linop.py
+++ gpmap/src/linop.py
@@ -17,6 +17,7 @@ from gpmap.src.matrix import (calc_cartesian_product,
                               calc_tensor_product_dot, calc_tensor_product_quad, 
                               inner_product, kron_dot, diag_pre_multiply)
 from numpy.linalg.linalg import norm
+from scipy.linalg.decomp import eigh_tridiagonal
 
 
 class ExtendedLinearOperator(_CustomLinearOperator):
@@ -134,40 +135,70 @@ class ExtendedLinearOperator(_CustomLinearOperator):
             if full_orth:
                 r_j -= Q[:, :-1].dot(Q[:, :-1].T.dot(r_j))
             
-            q_j_1 = q_j
             r_norm = norm(r_j)
-            q_j = r_j / r_norm
-            beta = r_norm
-            
             if np.allclose(r_norm, 0, atol=np.finfo(q_j.dtype).eps):
                 return(Q, T[:j+1, :][:, :j+1])
             
+            q_j_1 = q_j
+            q_j = r_j / r_norm
+            beta = r_norm
+            
         return(Q, T[:, :-1][:-1, :])
     
-    def calc_log_det(self, method='barry_pace99', n_vectors=10, degree=None):
+    def get_slq_config(self, lambda_min, lambda_max, epsilon=0.01, eta=0.05):
+        kappa = lambda_max / lambda_min
+        k1 = lambda_max * np.sqrt(kappa) * np.log(lambda_min + lambda_max)
+        degree = np.int(np.sqrt(kappa) / 4 * np.log(k1 / epsilon))
+        n_vectors = int(24 / epsilon ** 2 * np.log(1 + kappa) ** 2 * np.log(2 / eta))
+        return(n_vectors, degree)
+    
+    def calc_log_det(self, method='SLQ', n_vectors=10, degree=None,
+                     epsilon=0.01, eta=0.05, lambda_min=None, lambda_max=None):
         if method == 'naive':
-            sign, log_det = np.log(np.linalg.slogdet(self.todense()))
+            sign, log_det = np.linalg.slogdet(self.todense())
             msg = 'Negative determinant found. Ensure LinearOperator has positive eigenvalues'
             check_error(sign > 0, msg=msg)
             return(log_det)
+        
+        elif method == 'SLQ':
+            log_det = 0
+            if lambda_min is not None and lambda_max is not None:
+                n_vectors, degree = self.get_slq_config(lambda_min, lambda_max,
+                                                        epsilon=epsilon, eta=eta)
+            
+            for _ in range(n_vectors):
+                u = 0.5 - (np.random.uniform(size=self.shape[0]) > 0.5).astype(float)
+                T = self.lanczos(u, degree, return_Q=False)[1]
+#                 print(np.diag(T), np.diag(T, 1))
+                Theta, Y = eigh_tridiagonal(np.diag(T), np.diag(T, 1))
+                tau = Y[0, :]
+                log_det += np.sum([np.log(theta_k) * tau_k ** 2
+                                   for tau_k, theta_k in zip(tau, Theta)])
+            log_det = log_det * self.shape[0] / n_vectors
+            return(log_det)
+        
         else:
             upper = self.calc_eigenvalue_upper_bound()
             alpha = 1 / upper
             if degree is None:
                 degree = np.log(self.shape[0])
+            
             mat_log = TruncatedMatrixLog(self, degree, alpha)
             if method == 'barry_pace99':
                 v_i = mat_log.calc_trace_hutchinson(n_vectors)
-                log_det = v_i.mean()
-                err = self.shape[0] * alpha ** (degree - 1) / (degree + 1) /(1 - alpha)
-                print(err, degree, alpha)
-                err += 1.96 * np.std(v_i) / np.sqrt(n_vectors)
-                bounds = (log_det - err, log_det + err)
-                return(log_det, bounds)
+                log_det = self.shape[0] * v_i.mean()
+#                 err = self.shape[0] * alpha ** (degree - 1) / (degree + 1) /(1 - alpha)
+#                 err += 1.96 * np.std(v_i) / np.sqrt(n_vectors)
+#                 bounds = (log_det - err, log_det + err)
+                return(log_det)
             
             elif method == 'martin93':
                 return(mat_log.calc_trace(exact=True))
-                
+            
+            else:
+                msg = 'Unknown method for log_det estimation: {}'.format(method)
+                raise ValueError(msg)
+            
 
 class MatrixPolynomial(ExtendedLinearOperator):
     def __init__(self, linop, coeffs=None):
@@ -192,7 +223,7 @@ class MatrixPolynomial(ExtendedLinearOperator):
     def calc_trace_hutchinson(self, n_vectors):
         trace = []
         for _ in range(n_vectors):
-            v = np.random.normal(size=self.shape[1])
+            v = 0.5 - (np.random.uniform(size=self.shape[1]) > 0.5).astype(float)
             power = v
             trace_i = 0
             for c in self.coeffs[1:]:
