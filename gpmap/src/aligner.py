issues with the gradient of the regularized kernel alignment fixed

gpmap/src/aligner.py
@@ -36,8 +36,9 @@ class VCKernelAligner(object):
     
     def set_data(self, covs, distances_n, sigma2=0):
         D_n = np.diag(distances_n)
-        self.A = self.W_kd @ D_n @ self.W_kd.T
-        self.b = np.dot(covs, D_n @ self.W_kd.T)
+        WD = self.W_kd @ D_n
+        self.A = WD @ self.W_kd.T
+        self.b = WD @ covs - (sigma2 * self.A).sum(1)
         self.c = np.dot(covs, D_n @ covs)
         self.sigma2 = sigma2
     
@@ -58,16 +59,20 @@ class VCKernelAligner(object):
         if beta is None:
             beta = self.beta
 
-        lambdas = safe_exp(log_lambdas) + self.sigma2
+        lambdas = safe_exp(log_lambdas)
         Av = self.A @ lambdas
-        loss = self.c + np.dot(lambdas, Av) - 2 * lambdas.dot(self.b)
+        loss = self.c + np.dot(lambdas, Av - 2 * self.b)
 
         if beta > 0:
-            loss += beta * quad(self.second_order_diff_matrix, log_lambdas[1:])
+            reg_Av = self.second_order_diff_matrix @ log_lambdas[1:]
+            reg = beta * np.dot(reg_Av, log_lambdas[1:])
+            loss += reg
 
-        if return_grad and self.beta == 0:
-            # TODO: update for regularized version and for sigma2 != 0
-            grad = (2 * Av - 2 * self.b) * (lambdas)
+        if return_grad:
+            with np.errstate(over = 'ignore'):
+                grad = (2 * Av - 2 * self.b) * lambdas
+            if beta > 0:
+                grad += np.append([0], 2 * reg_Av)
             return(loss, grad)
 
         return(loss)
@@ -106,15 +111,17 @@ class VCKernelAligner(object):
         '''
         self.set_data(covs, ns, sigma2=sigma2)
         
-        res = lsq_linear(self.A, self.b, bounds=(sigma2, np.inf), method='bvls')
-        lambdas = res.x - sigma2
+        res = lsq_linear(self.A, self.b, bounds=(0, np.inf), method='bvls')
+        lambdas = res.x
 
         if self.beta > 0:
-            log_lambda0 = np.log(lambdas)
-            res = minimize(fun=self.calc_loss, x0=log_lambda0, method='Powell',
-                            options={'xtol': 1e-8, 'ftol': 1e-8})
+            log_lambda0 = np.log(lambdas + 1e-16)
+            res = minimize(fun=self.calc_loss, jac=True,
+                           x0=log_lambda0, #method='powell',
+                           args=(self.beta, True), 
+                        #    options={'maxiter': 1000, 'tol': 1e-16},
+                           )
             lambdas = np.exp(res.x)
-            
         return(lambdas)
     
     def predict(self, lambdas):

gpmap/test/test_aligner.py
@@ -49,6 +49,7 @@ class KernelAlignerTest(unittest.TestCase):
     def test_vc_kernel_alignment(self):
         # Simulate data
         np.random.seed(1)
+        beta = 1e5
         sigma2 = 0.1
         a, l, rho = 4, 5, 0.5
         P = 5 * RhoProjectionOperator(a, l, rho=rho).matrix_sqrt()
@@ -56,34 +57,49 @@ class KernelAlignerTest(unittest.TestCase):
         cov_true, ns = calc_covariance_distance(y_true, a, l)
         lambdas_true = calc_variance_components(y_true, a, l)
         y = np.random.normal(y_true, np.sqrt(sigma2))
+        cov_obs, ns = calc_covariance_distance(y, a, l)
 
         # Define kernel aligner and fit unregularized model
         aligner = VCKernelAligner(a, l)
-        lambdas_star = aligner.fit(cov_true, ns)
-        cov_pred = aligner.predict(lambdas_star)
-        loss, grad = aligner.calc_loss(np.log(lambdas_star), return_grad=True)
+        lambdas_star_1 = aligner.fit(cov_true, ns)
+        cov_pred = aligner.predict(lambdas_star_1)
+        loss, grad = aligner.calc_loss(np.log(lambdas_star_1), return_grad=True)
         assert(loss < 1e-12)
         assert(np.allclose(grad, 0, rtol=1e-10))
         assert(np.allclose(cov_true, cov_pred, rtol=0.01))
-        assert(np.allclose(lambdas_true, lambdas_star, rtol=0.5))
+        assert(np.allclose(lambdas_true, lambdas_star_1, rtol=0.5))
         
         # Align with beta > 0
-        aligner = VCKernelAligner(a, l, beta=0.01)
-        lambdas_star = aligner.fit(cov_true, ns)
-        cov_pred = aligner.predict(lambdas_star)
+        aligner = VCKernelAligner(a, l, beta=beta)
+        lambdas_star_2 = aligner.fit(cov_true, ns)
+        cov_pred = aligner.predict(lambdas_star_2)
         assert(np.allclose(cov_true, cov_pred, rtol=0.01))
-        assert(np.allclose(lambdas_true, lambdas_star, rtol=0.5))
+        assert(np.allclose(lambdas_true, lambdas_star_2, rtol=0.5))
+
+        # Ensure loss is lower than unregularized fit
+        loss1 = aligner.calc_loss(np.log(lambdas_star_1))
+        loss2 = aligner.calc_loss(np.log(lambdas_star_2))
+        assert(loss2 < loss1)
 
         # Add known measurement error sigma^2
-        cov_obs, ns = calc_covariance_distance(y, a, l)
         aligner = VCKernelAligner(a, l)
         lambdas_star_1 = aligner.fit(cov_obs, ns)
         lambdas_star_2 = aligner.fit(cov_obs, ns, sigma2=sigma2)
-
         cov_obs_pred = aligner.predict(lambdas_star_2 + sigma2)
         assert(not np.allclose(lambdas_star_1, lambdas_star_2, rtol=0.05))
         assert(np.allclose(cov_obs, cov_obs_pred, rtol=0.05))
         assert(np.allclose(lambdas_true, lambdas_star_2, rtol=0.5))
+
+        # Align with beta > 0
+        aligner = VCKernelAligner(a, l, beta=beta)
+        lambdas_star_3 = aligner.fit(cov_obs, ns, sigma2=sigma2)
+        cov_pred = aligner.predict(lambdas_star_3)
+        assert(np.allclose(lambdas_true, lambdas_star_3, rtol=0.5))
+
+        # Ensure loss is lower than unregularized fit
+        loss2 = aligner.calc_loss(np.log(lambdas_star_2))
+        loss3 = aligner.calc_loss(np.log(lambdas_star_3))
+        assert(loss3 < loss2)
         
     def test_rho_kernel_alignment(self):
         # Simulate data