bug in occupancy times calculation fixed. Entry rates renamed to absorption probability for correct nomenclature

gpmap/randwalk.py
@@ -122,13 +122,16 @@ class RandomWalk:
         idx = np.full(Q.shape[0], True)
         idx[states_idxs] = False
 
-        Q_tilde = Q[idx, :][:, idx]
+        Q_tilde = Q[idx, :][:, idx].T
 
-        n = Q_tilde.shape[0]
+        n = Q_tilde.shape[1]
         if pi is None:
             pi = np.full(n, fill_value=1.0 / n)
         if pi.shape[0] != n:
             msg = "pi must have the same length as the number of states in A"
+            raise ValueError(msg)
+        if not np.isclose(pi.sum(), 1):
+            msg = "pi must sum to 1"
             raise ValueError(msg)
 
         b = -pi
@@ -237,7 +240,7 @@ class TimeReversibleRandomWalk(RandomWalk):
         D_sqrt = DiagonalOperator(stat_pi_sqrt)
         D_sqrt_inv = DiagonalOperator(1.0 / stat_pi_sqrt)
 
-        S_tilde = self.sandwich_rate_matrix[idx, :][:, idx]
+        S_tilde = self.sandwich_rate_matrix[idx, :][:, idx].T
         S_tilde_inv = InverseOperator(S_tilde, method="cg", atol=1e-16)
 
         n = S_tilde.shape[0]
@@ -248,17 +251,21 @@ class TimeReversibleRandomWalk(RandomWalk):
                 "pi must have the same length as the number of states not in A"
             )
             raise ValueError(msg)
-        m_tilde = -D_sqrt_inv @ S_tilde_inv @ D_sqrt @ pi
+        if not np.isclose(pi.sum(), 1):
+            msg = "pi must sum to 1"
+            raise ValueError(msg)
+
+        m_tilde = -D_sqrt @ S_tilde_inv @ D_sqrt_inv @ pi
 
         m = np.zeros(self.space.n_states)
         m[idx] = m_tilde
         return pd.Series(m, index=self.space.state_labels)
 
-    def calc_entry_rates(self, state_labels, pi=None):
+    def calc_absorption_probabilities(self, state_labels, pi=None):
         """
-        Calculates the rate at which the random walk enters the set of states
-        A defined by `state_labels` when starting at the probability distribution
-        pi over the states not in A via each state in A.
+        Calculates the absorption probabilities into the set of states
+        `state_labels` for each state in the set when starting at the
+        probability distribution `pi` over the states not in A.
 
         Parameters
         ----------
@@ -266,18 +273,19 @@ class TimeReversibleRandomWalk(RandomWalk):
             List of state labels for the set A.
         pi : array-like of shape (n_genotypes - n_states,)
             Starting probability distribution over the states not in A.
+            These values must sum to 1.
 
         Returns
         -------
-        v : pd.Series of shape (n_genotypes,)
-            Rate at which the random walk enters A through each state in the space.
+        p : pd.Series of shape (n_states,)
+            Probability is absorbed at A for each state in A.
         """
 
         idxs = self.space.get_state_idxs(state_labels).values
         m = self.calc_average_occupancy_times(state_labels, pi)
 
-        v = self.rate_matrix[:, idxs].T @ m
-        return pd.Series(v, index=state_labels)
+        p = self.rate_matrix[:, idxs].T @ m
+        return pd.Series(p, index=state_labels)
 
     def set_stationary_freqs(self, log_freqs):
         self.stationary_freqs = np.exp(log_freqs)

test/test_randwalk.py
@@ -13,7 +13,12 @@ from scipy.sparse import load_npz, csr_matrix
 from gpmap.datasets import DataSet
 from gpmap.settings import BIN_DIR
 from gpmap.space import CodonSpace, SequenceSpace, DiscreteSpace
-from gpmap.randwalk import WMWalk, ReactivePaths, PopulationSizeModel
+from gpmap.randwalk import (
+    RandomWalk,
+    WMWalk,
+    ReactivePaths,
+    PopulationSizeModel,
+)
 from gpmap.seq import get_seqs_from_alleles, translate_seqs
 
 
@@ -184,32 +189,45 @@ class RandomWalkTests(unittest.TestCase):
         mc.calc_rate_matrix()
         m = mc.calc_average_occupancy_times(ser4)
         assert np.allclose(m.loc[ser4], 0)
-        assert np.allclose(m.loc["AAC"], 0.039679)
+        assert np.allclose(m.loc["AAC"], 0.038581473)
 
-    def test_calc_entry_rates(self):
+        # Ensure time-reversible method gives the same result
+        m2 = RandomWalk.calc_average_occupancy_times(mc, ser4)
+        assert np.allclose(m, m2)
+
+    def test_calc_absorption_probabilities(self):
         A = csr_matrix([[0, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0]])
         y = np.array([1, 1, 1, 1])
         space = DiscreteSpace(A, y=y, state_labels=["A", "B", "C", "D"])
         mc = WMWalk(space, Ns=1)
         mc.calc_rate_matrix()
-        m = mc.calc_entry_rates(["C", "D"])
+        m = mc.calc_absorption_probabilities(["C", "D"])
         assert np.allclose(m, [0.5, 0.5])
+        assert np.allclose(m.sum(), 1.0)
 
         # With the Ser codon landscape
         ser = ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]
-        mc = WMWalk(CodonSpace(["S"], add_variation=True, seed=0), Ns=1)
-        mc.calc_rate_matrix()
+        space = CodonSpace(["S"], add_variation=True, seed=0)
+        mc = WMWalk(space)
+        mc.calc_rate_matrix(Ns=1)
+        m = mc.calc_absorption_probabilities(ser)
+        assert np.allclose(m.shape[0], 6)
+        assert np.allclose(m.sum(), 1.0)
+        assert np.allclose(m.loc["AGC"], 0.199668)
+        assert np.allclose(m.loc["TCC"], 0.144478)
+
         pi = np.array(
             [
-                0 if x in ["TAA", "TAG", "TGA"] else 1.0 / 61
+                0 if x in ["TAA", "TAG", "TGA"] else 1.0 / 55
                 for x in mc.space.genotypes
                 if x not in ser
             ]
         )
-        m = mc.calc_entry_rates(ser, pi=pi)
+        m = mc.calc_absorption_probabilities(ser, pi=pi)
+        assert np.allclose(m.sum(), 1.0)
         assert np.allclose(m.shape[0], 6)
-        assert np.allclose(m.loc["AGC"], 0.190877)
-        assert np.allclose(m.loc["TCC"], 0.130535)
+        assert np.allclose(m.loc["AGC"], 0.201986)
+        assert np.allclose(m.loc["TCC"], 0.145702)
 
     def test_calc_hitting_prob_through(self):
         # Ensure right results when the probability is clearly 0.5 in a small